670467bb928d0539e8aff6bf8565d34a95507cfa
danmayer4l <dannymayer@Dannys-MacBook-Pro.local>
2018-07-14 13:32:14 -0500

    edited filed structure

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,27 +1,32 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-import Logo from '../assets/mmc_logo.png'
+import Logo from '../../assets/mmc_logo.png'
 
 const Header = () => (
-  <div
-  style={{
+  <div style={{
     background: '#FEFFFF'
-  }}
-  >
-    <div>
-      <img src={Logo} alt=""/>
-    </div>
-    <div>
+  }}>
+    <div className='nav-links'>
       <Link>About |</Link>
       <Link>Services |</Link>
       <Link>Blog |</Link>
       <Link>Contact</Link>
     </div>
-    <h1>
+    <div>
+      <img
+        src={Logo}
+        alt=""
+        style={{
+        height: 100,
+        width: 100,
+        margin: 25
+      }}/>
+    </div>
+    <h1 className='hero-title'>
       Mayer Media Co.
     </h1>
-    <h3>
+    <h3 className='hero-sub'>
       Design | Development | Marketing
     </h3>
   </div>
