811832f1dc2a7a47b823670f206f833b2a40b086
danmayer4l <dannymayer@Dannys-MacBook-Pro.local>
2018-07-17 20:13:43 -0500

    added contact form

---
+++ src/components/contact-form/contact-form.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+import './styles.css'
+
+const ContactForm = () => (
+    <form
+      name="contact"
+      method="post"
+      action="/thanks/"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field">
+
+      <input type="hidden" name="form-name" value="contact"/>
+      <p hidden>
+        <label>
+          Don’t fill this out:{" "}
+          <input name="bot-field"/>
+        </label>
+      </p>
+
+      <span className="control">
+        <input className="input" type="text" placeholder="First Name"/>
+      </span>
+      <span className="control">
+        <input className="input" type="text" placeholder="Last Name"/>
+      </span>
+
+      <span className="control">
+        <input className="input" type="email" placeholder="Email"/>
+      </span>
+      <span className="control">
+        <input className="input" type="text" placeholder="Phone"/>
+      </span>
+
+        <span className="control">
+          <input className="input" type="text" placeholder="Website"/>
+        </span>
+        <span className="control">
+          <input className="input" type="text" placeholder="Company"/>
+        </span>
+
+        <span className="control">
+          <textarea className="textarea" placeholder="Tell me about your project" rows="10"/>
+        </span>
+      <span className='control'>
+      <input className='submit-button' type="submit" value="Submit"/>
+      </span>
+    </form>
+)
+
+export default ContactForm
--- src/components/page-content/contact/contact.js
+++ src/components/page-content/contact/contact.js
@@ -1,27 +1,19 @@
 import React from 'react'
 
-import './styles.css'
+import ContactForm from '../../contact-form/contact-form'
 
-import Computer from '../../../assets/computer.png'
+import './styles.css'
 
 const contact = () => (
   <div className='container'>
-    <div className='wrapper'>
-      <div className='column'>
-        <div className='card'>
-          <hr className='border' />
-          <div className='card-title'>
-            <h3>Contact</h3>
-          </div>
-          <p className='card-content'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, saepe vel. In accusamus enim perspiciatis sunt explicabo temporibus, quidem id quisquam aliquam voluptatibus eaque animi culpa veniam magni, distinctio autem corporis commodi adipisci illum non repellendus ipsa saepe dicta. Voluptatibus inventore voluptatum quo! Autem laboriosam repudiandae, deleniti voluptate optio maxime!
-          </p>
-        </div>
-      </div>
-      <div className='column'>
-        <img src={Computer} alt='' />
-      </div>
+    <div className='message'>
+      <h1>Get in touch</h1>
+      <p>
+        Tell me about what you would like to accomplish with your brand.
+      </p>
     </div>
+    <hr className='border' />
+    <ContactForm />
   </div>
 )
 
